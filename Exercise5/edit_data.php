37829a2a4aaae85b05a5351749ce310e8a5d61d0
roxan romero <romeroroxan@gmail.com>
2016-12-06 09:50:58 +0800

    Changing href index.php to Home.php

--- Exercise5/edit_data.php
+++ Exercise5/edit_data.php
@@ -28,7 +28,7 @@ if(isset($_POST['btn-update']))
   ?>
   <script type="text/javascript">
   alert('Data Are Updated Successfully');
-  window.location.href='index.php';
+  window.location.href='Home.php';
   </script>
   <?php
  }
