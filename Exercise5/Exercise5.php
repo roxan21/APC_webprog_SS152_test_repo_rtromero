8fdadaaf80f724098748217d7bd290526ca16dbc
roxan romero <romeroroxan@gmail.com>
2016-12-13 10:54:52 +0800

    Successful debugging Exercise5.php

--- Exercise5/Exercise5.php
+++ Exercise5/Exercise5.php
@@ -4,8 +4,8 @@ include_once 'dbconfig.php';
 if(isset($_POST['btn-save']))
 {
  // variables for input data
- $first_name = $_POST['first_name'];
- $last_name = $_POST['last_name'];
+ $name = $_POST['name'];
+ $email = $_POST['email'];
  $nickname = $_POST['nickname'];
  $user_city = $_POST['user_city'];
  $gender = $_POST['gender'];
@@ -15,11 +15,24 @@ if(isset($_POST['btn-save']))
  
  // sql query for inserting data into database
  
-        $sql_query = "INSERT INTO users(first_name,last_name,nickname,user_city,gender,cp_num,comments) VALUES('$first_name','$last_name','$nickname','$user_city','$gender','$cp_num','$comments')";
- $con=mysqli_query($con,$sql_query);
-        
-        // sql query for inserting data into database
- 
+        $sql_query = "INSERT INTO users(name,email,nickname,user_city,gender,cp_num,comments) VALUES('$name','$email','$nickname','$user_city','$gender','$cp_num','$comments')";
+ if(mysqli_query($con,$sql_query))
+ {
+  ?>
+  <script type="text/javascript">
+  alert('Data Are Updated Successfully');
+  window.location.href='Exer5.php';
+  </script>
+  <?php
+ }
+ else
+ {
+  ?>
+  <script type="text/javascript">
+  alert('error occured while adding data');
+  </script>
+  <?php
+ }
 }
 ?>
 <html>
@@ -65,8 +78,8 @@ if(isset($_POST['btn-save']))
 
 <?php
 // define variables and set to empty values
-$nicknameErr = $nameErr = $emailErr = $genderErr = $addressErr = $phoneErr = "";
-$nickname = $name = $email = $gender = $comment = $address = $phone = "";
+$nicknameErr = $nameErr = $emailErr = $genderErr = $user_cityErr = $cp_numErr = "";
+$nickname = $name = $email = $gender = $comments = $user_city = $cp_num = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["name"])) {
@@ -78,7 +91,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $nameErr = "Only letters, numbers, and white space allowed"; 
     }
   }
-  
+
   if (empty($_POST["nickname"])) {
     $nicknameErr = "Nickname is required";
   } else {
@@ -99,10 +112,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
   }
     
-  if (empty($_POST["address"])) {
-    $address = "";
+  if (empty($_POST["user_city"])) {
+    $user_city = "";
   } else {
-    $address = test_input($_POST["address"]);
+    $user_city = test_input($_POST["user_city"]);
   }
 
   if (empty($_POST["gender"])) {
@@ -111,20 +124,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $gender = test_input($_POST["gender"]);
   }
   
-   if (empty($_POST["phone"])) {
-    $phoneErr = "Cell Phone is required";
+   if (empty($_POST["cp_num"])) {
+    $cp_numErr = "Cell Phone is required";
   } else {
-    $phone = test_input($_POST["phone"]);
+    $cp_num = test_input($_POST["cp_num"]);
     // check if name only contains letters and whitespace
-    if (!preg_match("/^[0-9 ]*$/",$phone)) {
-      $phoneErr = "Only letters and white space allowed"; 
+    if (!preg_match("/^[0-9 ]*$/",$cp_num)) {
+      $cp_numErr = "Only letters and white space allowed"; 
     }
   } 
   
-    if (empty($_POST["comment"])) {
-    $comment = "";
+    if (empty($_POST["comments"])) {
+    $comments = "";
   } else {
-    $comment = test_input($_POST["comment"]);
+    $comments = test_input($_POST["comments"]);
   }
 }
 
@@ -148,37 +161,21 @@ function test_input($data) {
   E-mail: <input type="text" name="email" value="<?php echo $email;?>">
   <span class="error">* <?php echo $emailErr;?></span>
   <br><br>
-  Address: <input type="text" name="address" value="<?php echo $address;?>">
+  Address: <input type="text" name="user_city" value="<?php echo $user_city;?>">
   <br><br>
   Gender:
   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
   <span class="error">* <?php echo $genderErr;?></span>
   <br><br>
-  Cell Phone: <input type="text" name="phone" value="<?php echo $phone;?>">
-  <span class="error">* <?php echo $phoneErr;?></span>
+  Cell Phone: <input type="text" name="cp_num" value="<?php echo $cp_num;?>">
+  <span class="error">* <?php echo $cp_numErr;?></span>
   <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  Comment: <textarea name="comments" rows="5" cols="40"><?php echo $comments;?></textarea>
   <br><br>
   <input type="submit" name="btn-save" value="Submit">  
 </form>
 
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $nickname;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $address;
-echo "<br>";
-echo $gender;
-echo $phone;
-echo "<br>";
-echo $comment;
-echo "<br>";
-?>
 <center>
 
 </body>
