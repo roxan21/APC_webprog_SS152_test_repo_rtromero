57540ab5b5506d4bc1bf55e6a3161c5519e2324f
roxan romero <romeroroxan@gmail.com>
2016-12-13 09:43:19 +0800

    Changing the htm file to php

---
+++ Exercise5/Gallery.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<title>Roxan Romero</title>
+<body>
+<style>
+	body{
+		font-family: Comic Sans MS;
+		background-image: url("minimal.jpg");
+    background-repeat: no-repeat;
+   	background-attachment: fixed;
+    background-position: center;
+	}
+	
+	a:link {
+		color: black;
+		width: 100%;
+		text-decoration: none;
+	}
+	div {
+		text-align:center;
+	}
+	a:visited {
+    color: black;
+  }
+	a:hover {
+		color: brown;
+	}
+</style>
+
+<div id="link" style = "margin-top:2em">
+	<p>
+	<p>
+	<a href="Home.php">HOME</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="About Me.php">ABOUT ME</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="Gallery.php">GALLERY</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="Exer5.php">PHP</a>
+	</p>
+</div>
+
+<div style="margin-top:4em">
+<img src="img1.jpg" width="350" height="350"/>
+<img src="img2.jpg" width="350" height="350"/>
+<img src="img3.jpg" width="350" height="350"/>
+<br>
+<img src="img5.jpg" width="350" height="350"/>
+<img src="img4.jpg" width="350" height="350"/>
+<img src="img6.jpg" width="350" height="350"/>
+<br>
+<img src="img7.jpg" width="350" height="350"/>
+<img src="img8.jpg" width="350" height="350"/>
+<img src="img9.jpg" width="350" height="350"/>
+<br>
+<img src="img10.jpg" width="350" height="350"/>
+<img src="img11.jpg" width="350" height="350"/>
+
+</div>
+
+
+</body>
+</html>
