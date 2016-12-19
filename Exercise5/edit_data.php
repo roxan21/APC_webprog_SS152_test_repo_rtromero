0f593d2b19c15dce58b594b4b55cdbbed4726189
Roxan Romero <romeroroxan@gmail.com>
2016-12-19 19:09:44 +0800

    Fixing some error of edit_data.php

--- Exercise5/edit_data.php
+++ Exercise5/edit_data.php
@@ -49,18 +49,34 @@ if(isset($_POST['btn-cancel']))
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
-</head>
 <body>
+<style>
+	body{
+		font-family: Comic Sans MS;
+		background-image: url("minimal.jpg");
+    background-repeat: no-repeat;
+   	background-attachment: fixed;
+    background-position: center;
+	}
+	
+	a:link {
+		color: black;
+		width: 100%;
+		text-decoration: none;
+	}
+	div {
+		text-align:center;
+	}
+	a:visited {
+    color: black;
+  }
+	a:hover {
+		color: brown;
+	}
+</style>
 <center>
 
 <div id="header">
- <div id="content">
-    <label>CRUD Operations With PHP and MySql - By Cleartuts</label>
-    </div>
 </div>
 
 <div id="body">
@@ -77,7 +93,7 @@ if(isset($_POST['btn-cancel']))
     <td><input type="text" name="nickname" placeholder="Nickname" value="<?php echo $fetched_row['nickname']; ?>" required /></td>
     </tr>
 	<tr>
-    <td><input type="text" name="user_city" placeholder="Address" value="<?php echo $fetched_row['user_city']; ?>" required /></td>
+    <td><input type="text" name="user_city" placeholder="Address" value="<?php echo $fetched_row['user_city']; ?>" /></td>
     </tr>
 	<tr>
     <td><input type="text" name="gender" placeholder="Gender" value="<?php echo $fetched_row['gender']; ?>" required /></td>
