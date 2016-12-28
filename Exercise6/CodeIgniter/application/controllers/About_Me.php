f9b7eabcedd591c268cbde0b5ea3c29da374686d
Roxan Romero <romeroroxan@gmail.com>
2016-12-29 00:54:44 +0800

    My About me php file in my current webpage.

---
+++ Exercise6/CodeIgniter/application/controllers/About_Me.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class About_Me extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('About_Me');
+
+	}
+}
