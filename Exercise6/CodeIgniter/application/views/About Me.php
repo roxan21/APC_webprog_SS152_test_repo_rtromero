a1a4592095cb5b18e96a6a7b7532617408082225
Roxan Romero <romeroroxan@gmail.com>
2016-12-29 00:58:49 +0800

    My current About me php, i added into controllers folder.

---
+++ Exercise6/CodeIgniter/application/views/About Me.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<title>Roxan Romero</title>
+<body>
+<style>
+	body{
+		font-family: Comic Sans MS;
+		background-image: url("minimal.jpg");
+    background-repeat: no-repeat;
+   	background-attachment: fixed;
+    background-position: center;
+	}
+	
+	a:link {
+		color: black;
+		text-decoration: none;
+		width: 100%;
+
+	}
+	div {
+		text-align:center;
+	}
+	a:visited {
+    color: black;
+  }
+	a:hover {
+		color: brown;
+	}
+	table, td, th {
+		text-align:center;
+    border-collapse: collapse;
+		border: 1px dashed maroon;
+	}
+  td, th {
+		padding: 8px;
+		text-align: center;
+	}
+</style>
+
+<div id="link" style = "margin-top:2em">
+	<p>
+	<p>
+	<a href="<?php echo site_url('Home');?>">HOME</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="<?php echo site_url('About_Me');?>">ABOUT ME</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="<?php echo site_url('Gallery');?>">GALLERY</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="<?php echo site_url('users');?>">PHP</a>
+	</p>
+<div>
+<center>
+<table style="width:70%; margin-top:5em;margin-bottom:3em" id="t01";>
+  <tr>
+    <th>HOBBIES</th>
+    <th>INTERESTS</th>
+  </tr>
+<tr>
+    <td>Web Browsing</td>
+    <td>Long Drive</td>
+  </tr>
+  <tr>
+    <td>Listening Music</td>
+    <td>PC Modding</td>
+  </tr>
+    <tr>
+    <td>Playing Online games</td>
+    <td>Baking</td>
+  </tr>
+  <tr>
+    <td>Watching TV Series</td>
+    <td>Playing Sports such as Badminton and Table Tennis</td>
+  </tr>
+</table>
+</div >
+
+<div>
+<center>
+<table style="width:70%; margin-top:5em;margin-bottom:3em" id="t01";>
+  <tr>
+    <th>MY FAVORITE WEBSITES.</th>
+  </tr>
+<tr>
+    <td><a href="https://www.facebook.com/">Facebook</a></td>
+  </tr>
+  <tr>
+    <td><a href="https://twitter.com/">Twitter</a></td>
+  </tr>
+    <tr>
+    <td><a href="https://www.instagram.com/">Instagram</a></td>
+  </tr>
+  <tr>
+    <td><a href="http://www.9gag.com/">9gag</a></td>
+  </tr>
+</table>
+</div >
+
+<center>
+
+
+
+</body>
+</html>
